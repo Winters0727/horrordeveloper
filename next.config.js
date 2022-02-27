a2d12b9905e6c9aae186bc9574441be6d442a2e7
winters0727 <winters0727@gmail.com>
2022-02-27 15:10:53 +0900

    temp commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://winters0727.github.io/horror-developer"
+      : "",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
